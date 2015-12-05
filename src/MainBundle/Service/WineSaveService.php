09a81ab1a0451c25dadbd9535afb16815d1e91fd
Sebastian Superczynski <sebastian.supeczynski@redexperts.net>
2015-12-05 18:33:54 +0100

    wine angular

--- src/MainBundle/Service/WineSaveService.php
+++ src/MainBundle/Service/WineSaveService.php
@@ -12,11 +12,11 @@ use PhpAmqpLib\Message\AMQPMessage;
  */
 class WineSaveService extends SendToMLService implements ConsumerInterface
 {
-    const DEFAULT_URL = 'https://azureml.firebaseio.com/';
-    const DEFAULT_TOKEN = 'p8JsTwvf6W2QxEpNSecYI69Viw1zy8w1Gz7Hr31Y';
+    const DEFAULT_URL = 'https://azurewines.firebaseio.com/';
+    const DEFAULT_TOKEN = 'jhpmmtPidAHoxswYP2QWO213B0Qx2jUNZFeBivsQ';
     const DEFAULT_PATH = '/wine';
-    const MS_URL = 'https://ussouthcentral.services.azureml.net/workspaces/8cc62d04a53c415cb85e331b37682a91/services/e2a7b6358e30410bbeb9a03db413ba6b/execute?api-version=2.0&details=true';
-    const MS_TOKEN = 'jV53kXuYi345dJ726vKeq2ixQOPrq3+uINr5jOOeEecJqh5CuQv1zyRXeIP7/k1NPWPZEQj8aFCnjsz6Yh08ug==';
+    const MS_URL = 'https://ussouthcentral.services.azureml.net/workspaces/8cc62d04a53c415cb85e331b37682a91/services/df8e2afa0b6844f598a8f80acfb4f626/execute?api-version=2.0&details=true';
+    const MS_TOKEN = 'sOgsy96HZKYCYoMLa7s2CUdZ3zRizErHT9Rh84HQMV7mxm3X5VAf6GtwHpzViTXbL4iNCE9ecAEymaDvHbFI9w==';
 
     /**
      * @param EntityManager $em
@@ -33,26 +33,39 @@ class WineSaveService extends SendToMLService implements ConsumerInterface
     public function execute(AMQPMessage $msg)
     {
         $data = json_decode($msg->body);
+
         $body = <<<JSON
 {
   "Inputs": {
     "input1": {
       "ColumnNames": [
-        "edible",
-        "cap-shape",
-        "cap-color",
-        "odor",
-        "gill-color",
-        "stalk-color"
+        "fixed acidity",
+        "volatile acidity",
+        "citric acid",
+        "residual sugar",
+        "chlorides",
+        "free sulfur dioxide",
+        "total sulfur dioxide",
+        "density",
+        "pH",
+        "sulphates",
+        "alcohol",
+        "quality"
       ],
       "Values": [
         [
-            "$data->edible",
-            "$data->capShape",
-            "$data->capColor",
-            "$data->odor",
-            "$data->gillColor",
-            "$data->stalkColor",
+            "$data->fixedAcidity",
+            "$data->volatileAcidity",
+            "$data->citricAcidity",
+            "$data->residualSugar",
+            "$data->chlorides",
+            "$data->freeSugar",
+            "$data->totalSugar",
+            "$data->density",
+            "$data->pH",
+            "$data->sulphates",
+            "$data->alcohol",
+            "$data->quality"
         ]
       ]
     }
@@ -65,14 +78,19 @@ JSON;
         $data = json_decode($response);
         $array = array_combine($data->Results->output1->value->ColumnNames, $data->Results->output1->value->Values[0]);
         $formattedArray = [
+            'fixedAcidity' => $array['fixed acidity'],
+            'volatileAcidity' => $array['volatile acidity'],
+            'citricAcidity' => $array['citric acid'],
+            'residualSugar' => $array['residual sugar'],
+            'chlorides' => $array['chlorides'],
+            'freeSugar' => $array['free sulfur dioxide'],
+            'totalSugar' => $array['total sulfur dioxide'],
+            'density' => $array['density'],
+            'pH' => $array['pH'],
+            'sulphates' => $array['sulphates'],
+            'alcohol' => $array['alcohol'],
+            'quality' => $array['quality'],
             'result' => $array['Scored Labels'],
-            'percent' => $array['Scored Probabilities'],
-            'capColor' => $array['cap-color'],
-            'capShape' => $array['cap-shape'],
-            'edible' => $array['edible'],
-            'gillColor' => $array['gill-color'],
-            'stalkColor' => $array['stalk-color'],
-            'odor' => strtolower($array['odor']),
         ];
         $this->sendToFirebase($formattedArray, self::DEFAULT_URL, self::DEFAULT_TOKEN, self::DEFAULT_PATH);
     }
